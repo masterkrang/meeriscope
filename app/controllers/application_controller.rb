680d5e5194ea42da7da00576a04453e00cdbcd8a
spidergears <findspidergears@gmail.com>
2015-08-15 14:54:12 +0530

    Added new layout for devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  layout :layout_by_resource
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
@@ -10,6 +11,16 @@ class ApplicationController < ActionController::Base
       home_index_path
     else
       stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
     end
   end
 
