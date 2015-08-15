4e3ace39e73ada60c2c4988733112fd037c61212
Timur Kunayev <timur.kunayev@gmail.com>
2015-08-15 10:00:12 -0600

    added date validator

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -6,4 +6,18 @@ class StreamsController < ApplicationController
   def new
     @stream = Stream.new
   end
+
+  def create
+    @stream = Stream.new stream_params.merge(user_id: current_user.id)
+    if @stream.valid?
+      @stream.save!
+      redirect_to streams_path
+    else
+      render :new
+    end
+  end
+
+  def stream_params
+    params.require(:stream).permit(:title, :desciption, :location, :start_time, :category, :goal, :price)
+  end
 end
--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,2 +1,6 @@
 class Stream < ActiveRecord::Base
+  validates :title, :description, :start_time, :goal, :price, presence: true
+
+  validates :start_time, date: {on_or_after: Date.today + 16.days}
+  
 end
